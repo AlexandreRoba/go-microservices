09028439559dd80d79140c8a72e76caf83acb062
Alexandre Roba <alexandre.roba@arobanet.be>
2017-08-02 22:01:58 +0200

    Json marshalling

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+Go Microservice sample app
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,12 +10,29 @@ import (
 func main() {
 	port := 8080
 
-	http.HandleFunc("/helloworld", helloworldHandler)
+	http.HandleFunc("/helloworld", helloWorldHandler)
 
 	log.Printf("Server starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-func helloworldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world\n")
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	response := helloWorldResponse{Message: "Hello world!"}
+	data, err := json.Marshal(response)
+	if err != nil {
+		panic("Ooops")
+	}
+
+	fmt.Fprint(w, string(data))
+}
+
+type helloWorldResponse struct {
+	//change the output field name to message
+	Message string `json:"message"`
+	//Do not output the field
+	Author string `json:"-"`
+	//Do not output the field if it is empty
+	Date string `json:",omitempty"`
+	//Convert output to a string
+	Id int `json:"id,string"`
 }
